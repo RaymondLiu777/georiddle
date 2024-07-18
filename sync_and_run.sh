17ba220e51ed5f3dfba7804841a911478a0ef3bd
Raymond Liu <raymondliu4414@gmail.com>
2024-07-18 15:43:15 -0700

    Add script to sync files

---
+++ sync_and_run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Source the config file
+if [ -f config.sh ]; then
+  source config.sh
+else
+  echo "Configuration file not found!"
+  exit 1
+fi
+
+# Command line option to only sync
+sync_only=false
+
+# Parse command-line options
+while getopts "s" opt; do
+  case ${opt} in
+    s)
+      sync_only=true
+      ;;
+    *)
+      echo "Usage: $0 [-s]"
+      exit 1
+      ;;
+  esac
+done
+
+# Set up ssh-agent to avoid repeated queries to password
+eval "$(ssh-agent)"
+ssh-add
+
+# Sync directories
+unison "$SOURCE_DIR" "ssh://$SSH_HOST$DESTINATION_DIR" \
+  -ignore 'Path tmp/cache/' \
+  -ignore 'Path tmp/pids/' \
+  -ignore 'Path tmp/sockets/' \
+  -ignore 'Path tmp/sessions/' \
+  -ignore 'Path tmp/bootsnap/' \
+  -ignore 'Path log/' \
+  -ignore 'Name Gemfile.lock' \
+  -ignore 'Name .env*' \
+  -ignore 'Path public/assets/' \
+  -ignore 'Path public/packs/' \
+  -ignore 'Path .git/'
+
+# Run server
+if ! $sync_only; then
+  ssh -t $SSH_HOST "cd /Users/raymondliu$DESTINATION_DIR && bin/rails server"
+fi
